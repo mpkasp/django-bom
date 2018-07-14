5a73bc338913964164f9a24c8b6350777bd406d8
Mike Kasparian <mpkasp@gmail.com>
2018-07-14 16:07:03 -0500

    update readme and templates

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-bom',
-    version='0.71',
+    version='0.72',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
