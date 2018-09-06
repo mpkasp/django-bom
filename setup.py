b3c0a0b8d689f8d102269a6defe6efea49363707
Mike Kasparian <mpkasp@gmail.com>
2018-09-05 22:23:47 -0500

    change setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 2.1.1',
+        'Framework :: Django :: 2.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
