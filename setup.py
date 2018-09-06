9967eb2aebc78001e4a89c6b4b9d290d3bb34eae
Mike Kasparian <mpkasp@gmail.com>
2018-09-05 22:24:36 -0500

    edit setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7.0',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
