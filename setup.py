b463840652ca7a6afa4f6f58700ac1936821e030
Mike Kasparian <mpkasp@gmail.com>
2018-10-08 21:59:18 -0500

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-bom',
-    version='1.18',
+    version='1.19',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
