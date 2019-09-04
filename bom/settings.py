b7f460db951eb972f365130daf5732dfdf5be2b8
Mike Kasparian <mpkasp@gmail.com>
2019-09-03 21:58:21 -0500

    fix social auth pipeline

--- bom/settings.py
+++ bom/settings.py
@@ -188,7 +188,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
-    'bom.google_drive.initialize_parent',
+    'bom.third_party_apis.google_drive.initialize_parent',
 )
 
 SOCIAL_AUTH_DISCONNECT_PIPELINE = (
