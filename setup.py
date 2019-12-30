1002fd4701005a8bdec122a29dad786a6e5b8ea6
Mike Kasparian <mpkasp@gmail.com>
2019-12-30 15:52:35 -0500

    bug and docs fixin

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-bom',
-    version='1.121',
+    version='1.123',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
