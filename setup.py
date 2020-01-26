75e0a22759d33b17c4d32c2a00f06c52ad60a7bc
Mike Kasparian <mpkasp@gmail.com>
2020-01-26 14:48:55 -0600

    bump vers

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-bom',
-    version='1.139',
+    version='1.140',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
