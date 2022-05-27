6c649efecda73264bef0b96bedcc4e8ae49532a1
Mike Kasparian <mpkasp@gmail.com>
2022-05-27 17:37:21 -0400

    bump some packages to satisfy github security prs, thanks github

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-bom',
-    version='1.217',
+    version='1.219',
     packages=find_packages(),
     include_package_data=True,
     license='GPL 3.0 License',
